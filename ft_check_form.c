59f124a724f5d15176b6fea3ce7e4abe21b97997
Younes Hanafi <yohanafi@mi-r8-p3.s19.be>
2023-04-27 15:07:55 +0200

    test

--- ft_check_form.c
+++ ft_check_form.c
@@ -1,20 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft_printf.h                                     :+:      :+:    :+:   */
+/*   ft_check_form.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yohanafi <yohanafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/25 15:57:10 by yohanafi          #+#    #+#             */
-/*   Updated: 2023/04/27 14:04:14 by yohanafi         ###   ########.fr       */
+/*   Created: 2023/04/27 12:11:11 by yohanafi          #+#    #+#             */
+/*   Updated: 2023/04/27 14:37:09 by yohanafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_PRINTF_H
-# define LIBFT_PRINTF_H
-# include <stdarg.h>
-# include <stdio.h>
+#include "libft_printf.h"
 
-int ft_printf(const char *, ...);
-size_t ft_check_form(char c, va_list arg_lst);
-#endif
+size_t ft_check_form(char c, va_list arg_lst)
+{
+	size_t rlt;
+
+	rlt = 0;
+	if (c == 'c')
+		rlt += ft_putchar(va_arg(arg_lst, int));
+	if (c == 's')
+		rlt += ft_putstr(va_arg(arg_lst, char *));
+	if (c == 'd' || c == 'i')
+		rlt += ft_putnbr(va_arg(arg_lst, int));
+		
+}
--- ft_printf.c
+++ ft_printf.c
@@ -6,34 +6,32 @@
 /*   By: yohanafi <yohanafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 11:55:10 by yohanafi          #+#    #+#             */
-/*   Updated: 2023/04/26 14:33:14 by yohanafi         ###   ########.fr       */
+/*   Updated: 2023/04/27 14:01:26 by yohanafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_printf.h"
-const char *ft_check_form(const char *format)
-{
-	if (*format == 'c')
-		return ((const char *)'c');
-	else 
-		return (NULL);
-}
 
 int ft_printf(const char *format, ...)
 {
 	va_list arg_lst;
-	int		i;
-	int		rlt;
+	size_t	rlt;
 
-	i = 0;
+	va_start(arg_lst, format);
 	rlt = 0;
-	while (format[i])
+	while (*format)
 	{
-		if (format[i] == '%')
-			rlt	= (int)ft_check_form(&format[i + 1]);
-		i++;
+		if (*format == '%')
+		{
+			rlt += ft_check_form(*(format + 1), arg_lst);
+			format++;
+		}
+		else
+			rlt += ft_putchar(*format);
+		format++;	
 	}
-	return (rlt);
+	va_end(arg_lst);
+	return(rlt)
 }
 
 int main()
